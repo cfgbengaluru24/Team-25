89a1a196a2b26012beb6f73ed0f81c3a7e64c371
Mitansh Patel <mitanshpatel288@gmail.com>
2024-07-28 03:16:23 +0530

    link flask change

--- frontend/src/Trainee/Courses.jsx
+++ frontend/src/Trainee/Courses.jsx
@@ -11,10 +11,10 @@ const Courses = () => {
             <img className="rounded-t-lg w-full h-56 object-cover" src="https://d2xduyqs25ssfe.cloudfront.net/uploads/nrf.mykademy.com/items_org/course/67.webp?v=11" alt="" />
           </a>
           <div className="p-5">
-            <Link to={"http://localhost:5008"}>
+            <Link to={"http://localhost:5007"}>
               <h5 className="mb-2 text-lg font-bold tracking-tight text-gray-900 dark:text-white">Access your communication</h5>
             </Link>
-            <Link to={"http://localhost:5008"} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            <Link to={"http://localhost:5007"} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
               Take Assessment
               <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
